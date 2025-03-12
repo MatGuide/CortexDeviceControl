0fb4113b2e608b375bd93afade9d1439f044deca
MatGuide <mateusz.gajda.101@gmail.com>
2025-03-12 15:01:43 +0000

    Configuration and email alerting

--- CortexDeviceControl.py
+++ CortexDeviceControl.py
@@ -1,7 +1,12 @@
 import requests
 import json
+import smtplib
+import ssl
 from jinja2 import Template
 from datetime import datetime, timedelta
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 class DeviceControl:
 
@@ -21,9 +26,9 @@ class DeviceControl:
 
         self.sufix = ""
 
-    def get_device_violations(self):
+    def get_device_violations(self, interval):
  
-        past_time = int((datetime.utcnow() - timedelta(minutes=360)).timestamp() * 1000)
+        past_time = int((datetime.utcnow() - timedelta(minutes=interval)).timestamp() * 1000)
 
         self.sufix = "/device_control/get_violations" 
 
@@ -49,70 +54,102 @@ class DeviceControl:
             print(f"Error occured {e}.")
         else:
             violations = response.json()["reply"]["violations"]
-            try:
-                with open("violations.json", "w", encoding="utf-8") as file:
-                    json.dump(violations, file, ensure_ascii=False, indent=4)
-            except Exception as e:
-                print(f"Error occured {e}.")
+
+            if violations != []:   
+                try:
+                    with open("violations.json", "w", encoding="utf-8") as file:
+                        json.dump(violations, file, ensure_ascii=False, indent=4)
+                except Exception as e:
+                    print(f"Error occured {e}.")
+            else:
+                with open("violations.json", "w") as file:
+                    json.dump({}, file)
+            
 
     def create_html_report(self):
         
         data = []
 
         html_template = """
-            <!DOCTYPE html>
-            <html lang="pl">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Raport</title>
-                <style>
-                    body { font-family: Arial, sans-serif; margin: 20px; }
-                    h1 { color: #333; }
-                    table { width: 100%%; border-collapse: collapse; margin-top: 20px; }
-                    th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }
-                    th { background-color: #f2f2f2; }
-                </style>
-            </head>
-            <body>
-                <h1>Raport</h1>
-                <p>Data wygenerowania: {{ date }}</p>
-                <table>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nazwa</th>
-                        <th>Wartość</th>
-                    </tr>
-                    {% for item in data %}
-                    <tr>
-                        <td>{{ item.violation_id }}</td>
-                        <td>{{ item.hostname }}</td>
-                        <td>{{ item.username }}</td>
-                    </tr>
-                    {% endfor %}
-                </table>
-            </body>
-            </html>
-            """
+        <!DOCTYPE html>
+        <html lang="pl">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Raport</title>
+            <style>
+                body { font-family: Arial, sans-serif; margin: 20px; }
+                h1 { color: #333; }
+                table { width: 100%%; border-collapse: collapse; margin-top: 20px; }
+                th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }
+                th { background-color: #f2f2f2; }
+            </style>
+        </head>
+        <body>
+            <h1>Raport</h1>
+            <p>Data wygenerowania: {{ date }}</p>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Time</th>
+                    <th>Hostname</th>
+                    <th>Username</th>
+                    <th>Vendor</th>
+                    <th>Product</th>
+                    <th>Device SN</th>
+                </tr>
+                {% for item in data %}
+                <tr>
+                    <td>{{ item.violation_id }}</td>
+                    <td>{{ item.timestamp }}</td>
+                    <td>{{ item.hostname }}</td>
+                    <td>{{ item.username }}</td>
+                    <td>{{ item.vendor }}</td>
+                    <td>{{ item.product }}</td>
+                    <td>{{ item.serial }}</td>
+                </tr>
+                {% endfor %}
+            </table>
+        </body>
+        </html>
+        """
 
         with open('violations.json', 'r') as file:
             violations = json.load(file)
-
-        for violation in violations:
-            data.append({"violation_id": violation["violation_id"], "hostname": violation["hostname"], "username": violation["username"]})
         
-        template = Template(html_template)
-        html_report = template.render(date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), data=data)
-
-        with open("report.html", "w") as file:
-            file.write(html_report)
-
-        # for violation in violations:
-        #     data.append({"id": violation["violation_id"], "hostname": violation["hostname"], "username"})
-        
-        # print(data)
+        if violations != {}: 
+            for violation in violations:
+                timestamp = datetime.utcfromtimestamp(violation["timestamp"]/1000).strftime('%Y-%m-%d %H:%M:%S')            
+                data.append({"violation_id": violation["violation_id"], "timestamp": timestamp, "hostname": violation["hostname"], "username": violation["username"], \
+                "vendor": violation["vendor"], "product": violation["product"], "serial": violation["serial"]})
             
-    # def email_alert(self):
-    #     pass
+            template = Template(html_template)
+            html_report = template.render(date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), data=data)
 
+            with open("report.html", "w") as file:
+                file.write(html_report)
 
+            return True
+
+    def email_alert(self, server, port, sender_email, receiver_email, password):
+
+        try:
+            with open("report.html", "r") as file:
+                text = file.read()
+        except Exception as e:
+            print(f"Error occured {e}.")
+
+        message = MIMEMultipart()
+        message["Subject"] = "Device violation detected"
+        message["From"] = sender_email
+        message["To"] = ",".join(receiver_email)
+
+        message.attach(MIMEText(text, "html"))
+
+        with smtplib.SMTP(server, port) as server:
+            server.starttls()
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            server.quit()
+
+            
--- main.py
+++ main.py
@@ -8,7 +8,19 @@ api_url = os.getenv("API_URL")
 api_id = os.getenv("API_ID")
 api_key = os.getenv("API_KEY")
 
+server = os.getenv("EMAIL_SERVER_ADDRESS") 
+port = os.getenv("EMAIL_SERVER_PORT")
+sender = os.getenv("EMAIL_SENDER")
+password = os.getenv("EMAIL_PASSWORD")
+
+interval = int(os.getenv("INTERVAL"))
+
+receiver = ["mateusz.gajda.101@gmail.com"]
+
 if __name__ == "__main__":
     connection = DeviceControl(api_url, api_id, api_key)
-    connection.get_device_violations()
-    connection.create_html_report()
+
+    connection.get_device_violations(interval)
+    report = connection.create_html_report()
+    if report:
+        connection.email_alert(server, port, sender, receiver, password)
