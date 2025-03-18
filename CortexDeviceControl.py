fa729f904c5aeada10b2ad315fd5bf7d818bff15
MatGuide <mateusz.gajda.101@gmail.com>
2025-03-18 08:18:36 +0000

    Teams alerts updated

--- CortexDeviceControl.py
+++ CortexDeviceControl.py
@@ -23,11 +23,10 @@ class DeviceControl:
         }
 
         self.payload = {}
-
         self.sufix = ""
 
-    def get_device_violations(self, interval):
- 
+    def get_device_violations(self):
+        interval = 500
         past_time = int((datetime.utcnow() - timedelta(minutes=interval)).timestamp() * 1000)
 
         self.sufix = "/device_control/get_violations" 
@@ -59,6 +58,7 @@ class DeviceControl:
                 try:
                     with open("violations.json", "w", encoding="utf-8") as file:
                         json.dump(violations, file, ensure_ascii=False, indent=4)
+                        return violations
                 except Exception as e:
                     print(f"Error occured {e}.")
             else:
@@ -71,48 +71,48 @@ class DeviceControl:
         data = []
 
         html_template = """
-        <!DOCTYPE html>
-        <html lang="pl">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Raport</title>
-            <style>
-                body { font-family: Arial, sans-serif; margin: 20px; }
-                h1 { color: #333; }
-                table { width: 100%%; border-collapse: collapse; margin-top: 20px; }
-                th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }
-                th { background-color: #f2f2f2; }
-            </style>
-        </head>
-        <body>
-            <h1>Raport</h1>
-            <p>Data wygenerowania: {{ date }}</p>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Time</th>
-                    <th>Hostname</th>
-                    <th>Username</th>
-                    <th>Vendor</th>
-                    <th>Product</th>
-                    <th>Device SN</th>
-                </tr>
-                {% for item in data %}
-                <tr>
-                    <td>{{ item.violation_id }}</td>
-                    <td>{{ item.timestamp }}</td>
-                    <td>{{ item.hostname }}</td>
-                    <td>{{ item.username }}</td>
-                    <td>{{ item.vendor }}</td>
-                    <td>{{ item.product }}</td>
-                    <td>{{ item.serial }}</td>
-                </tr>
-                {% endfor %}
-            </table>
-        </body>
-        </html>
-        """
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raport</title>
+    <style>
+        body { font-family: Arial, sans-serif; margin: 20px; }
+        h1 { color: #333; }
+        table { width: 100%%; border-collapse: collapse; margin-top: 20px; }
+        th, td { border: 1px solid #ddd; padding: 8px; text-align: left; }
+        th { background-color: #f2f2f2; }
+    </style>
+</head>
+<body>
+    <h1>Raport</h1>
+    <p>Data wygenerowania: {{ date }}</p>
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Time</th>
+            <th>Hostname</th>
+            <th>Username</th>
+            <th>Vendor</th>
+            <th>Product</th>
+            <th>Device SN</th>
+        </tr>
+        {% for item in data %}
+        <tr>
+            <td>{{ item.violation_id }}</td>
+            <td>{{ item.timestamp }}</td>
+            <td>{{ item.hostname }}</td>
+            <td>{{ item.username }}</td>
+            <td>{{ item.vendor }}</td>
+            <td>{{ item.product }}</td>
+            <td>{{ item.serial }}</td>
+        </tr>
+        {% endfor %}
+    </table>
+</body>
+</html>
+"""
 
         with open('violations.json', 'r') as file:
             violations = json.load(file)
@@ -151,5 +151,5 @@ class DeviceControl:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
             server.quit()
+    
 
-            
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from CortexDeviceControl import DeviceControl
+from Teams import Teams
 from dotenv import load_dotenv
-import os
+import os, json
 
 load_dotenv()
 
@@ -11,16 +12,23 @@ api_key = os.getenv("API_KEY")
 server = os.getenv("EMAIL_SERVER_ADDRESS") 
 port = os.getenv("EMAIL_SERVER_PORT")
 sender = os.getenv("EMAIL_SENDER")
+receivers = json.loads(os.getenv("EMAIL_RECEIVERS", "[]"))
 password = os.getenv("EMAIL_PASSWORD")
 
 interval = int(os.getenv("INTERVAL"))
 
-receiver = [""]
+client_id = os.getenv("CLIENT_ID")
+username = os.getenv("USERNAME")
+teams_password = os.getenv("PASSWORD")
+
 
 if __name__ == "__main__":
     connection = DeviceControl(api_url, api_id, api_key)
-
-    connection.get_device_violations(interval)
+    violations = connection.get_device_violations()
     report = connection.create_html_report()
+
+    teams = Teams(client_id, username, teams_password)
+    teams.send_teams_alert(violations)
+    teams.send_teams_alert_to_team("Cortex XSIAM", "Cortex XSIAM")
     if report:
-        connection.email_alert(server, port, sender, receiver, password)
+        connection.email_alert(server, port, sender, receivers, password)
